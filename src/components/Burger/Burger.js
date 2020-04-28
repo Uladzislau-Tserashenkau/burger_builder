2b81dc89e00382715a2083ab893404a6aef2104e
Wlad <cosades@mail.ru>
2020-04-28 16:44:47 +0300

    making connection between burger and burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
+  const ingredientsComponentsArr = Object.keys(props.ingredients).map(
+    (igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    }
+  );
+  console.log(ingredientsComponentsArr);
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {ingredientsComponentsArr}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
