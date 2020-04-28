55642eabe43cef80479f9938bdfc47ecdcd15a38
Wlad <cosades@mail.ru>
2020-04-28 20:06:47 +0300

    added build controls to proj

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,22 @@
 import React from "react";
-import classes from "BuldControls.module.css";
+import classes from "./BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { type: "salad" },
+  { type: "meat" },
+  { type: "cheese" },
+  { type: "bacon" },
+];
 
 const BuildControls = (props) => {
-  return <div></div>;
+  return (
+    <div className={classes.BuildControls}>
+      {controls.map((ctrl) => (
+        <BuildControl key={ctrl.type} label={ctrl.type} />
+      ))}
+    </div>
+  );
 };
 
 export default BuildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/ax";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 export default class BurgerBuilder extends Component {
   state = {
@@ -16,7 +17,7 @@ export default class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build controls</div>
+        <BuildControls />
       </Aux>
     );
   }
