9f6ea726d12dbb3ccf93279c5a0a553d1b72f3a8
Wlad <cosades@mail.ru>
2020-05-01 20:07:48 +0300

    added a site logo at sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from "react";
 import Aux from "../../hoc/ax";
 import classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = (props) => {
   return (
     <Aux>
       <Toolbar />
+      <SideDrawer />
       <main className={classes.top_content}>{props.children}</main>
     </Aux>
   );
