365130b03c01cbd5b8a345f74dc60b2298de00b7
Wlad <cosades@mail.ru>
2020-05-01 19:46:37 +0300

    added navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Aux from "../../hoc/ax";
 import classes from "./Layout.module.css";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 
 const Layout = (props) => {
   return (
     <Aux>
-      <div className={classes.top_content}>Toolbar, Side, Backdrop</div>
-      <main>{props.children}</main>
+      <Toolbar />
+      <main className={classes.top_content}>{props.children}</main>
     </Aux>
   );
 };
