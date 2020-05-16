1a85c8349cfb937476eacbb1d9813ebed8a904eb
Wlad <cosades@mail.ru>
2020-05-17 00:53:21 +0300

    added a loading spinner on login and some error handling

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -33,7 +33,7 @@ export const auth = (mail, password, isSignUp) => {
       })
       .catch((err) => {
         console.log(err);
-        dispatch(authFail(err));
+        dispatch(authFail(err.response.data.error));
       });
   };
 };
