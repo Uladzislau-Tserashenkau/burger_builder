dd6d35553fc1f78f00864067c96b5221c387c18d
Wlad <cosades@mail.ru>
2020-06-09 17:15:34 +0300

    refactored drawer toggle

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,13 +1,13 @@
 import React from "react";
-import classes from "./DrawerToggle.module.css";
+import "./DrawerToggle.scss";
 import PropTypes from "prop-types";
 
 const DrawerToggle = (props) => {
   return (
-    <div onClick={props.clicked} className={classes.DrawerToggle}>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div onClick={props.clicked} className={"drawer-toggle"}>
+      <div className={"drawer-toggle__stick"}></div>
+      <div className={"drawer-toggle__stick"}></div>
+      <div className={"drawer-toggle__stick"}></div>
     </div>
   );
 };
