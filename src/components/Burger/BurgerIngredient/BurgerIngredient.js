776ff102244144029ff17fc762039ed2bfa32bad
Wlad <cosades@mail.ru>
2020-06-09 16:28:00 +0300

    added correct class names to burger ingredient

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -7,27 +7,27 @@ const burgerIngerdient = (props) => {
 
   switch (props.type) {
     case "bread-bottom":
-      ingredient = <div className={"burger_bread-bottom"}></div>;
+      ingredient = <div className={"burger__bread--bottom"}></div>;
       break;
     case "bread-top":
       ingredient = (
-        <div className={"burger_bread-top"}>
-          <div className={"burger_seeds1"}></div>
-          <div className={"burger_seeds2"}></div>
+        <div className={"burger__bread--top"}>
+          <div className={"burger__seeds1"}></div>
+          <div className={"burger__seeds2"}></div>
         </div>
       );
       break;
     case "meat":
-      ingredient = <div className={"burger_meat"}></div>;
+      ingredient = <div className={"burger__meat"}></div>;
       break;
     case "cheese":
-      ingredient = <div className={"burger_cheese"}></div>;
+      ingredient = <div className={"burger__cheese"}></div>;
       break;
     case "bacon":
-      ingredient = <div className={"burger_bacon"}></div>;
+      ingredient = <div className={"burger__bacon"}></div>;
       break;
     case "salad":
-      ingredient = <div className={"burger_salad"}></div>;
+      ingredient = <div className={"burger__salad"}></div>;
       break;
     default:
       ingredient = null;
