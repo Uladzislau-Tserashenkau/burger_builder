431da00cbc912eb09af03bf79e43385ff06e5fba
Wlad <cosades@mail.ru>
2020-06-12 12:52:32 +0300

    added a comment

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import thunk from "redux-thunk";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
-
+// comment
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
